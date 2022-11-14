2017d7429a58da81123efd4042efa7787634a1db
Dylan Doelling <blackice150@gmail.com>
2022-11-14 13:54:45 -0700

    it will now put into SQS

--- widget-request-handler.py
+++ widget-request-handler.py
@@ -6,18 +6,17 @@ sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
     body = event['body']
+    body2 = body.replace("'", "\"")
     print(type(body))
     print(body)
-    
+
     try:
-        sqs.send_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/302817204902/cs5260-requests', MessageBody=body)
+        sqs.send_message(QueueUrl='https://sqs.us-east-1.amazonaws.com/302817204902/cs5260-requests', MessageBody=body2)
     except Exception:
         logging.info("Could not put request into SQS from lambda.")
         raise Exception
-    
-    print("hello world")
         
     return {
         'statusCode': 200,
-        'body': json.dumps(bod)
+        'body': json.dumps(body)
     }
